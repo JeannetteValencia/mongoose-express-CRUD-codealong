5a007ae9930a5d1ce1489b79097a66313fd13f1d
jeannette.valencia <jfatima.valencia@gmail.com>
2021-10-18 17:19:06 +0200

    creating a form

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -8,12 +8,32 @@ router.get("/books", (req, res) => {
       const data = {
         booksArr: booksFromDB
       }
-      res.render ("/books/books-list", data)
+      console.log(">>>DATA>>>", data)
+      res.render ("books/books-list", data)
     })
     .catch((err)=>{
       res.send("An error has occurred", err)
       next(err);
     })
 } )
+
+router.get ('/books/create', (req, res, next)=>{
+  res.render("books/book-create");
+})
+
+router.get("/books/:bookId", (req, res, next)=>{ //params
+  Book
+    .findById(req.params.bookId)
+    .then((bookById)=>{
+       const details = {
+         booksArr: bookById
+       }
+       res.render("books/books-details", details) //no slash HERE!!!!
+    })
+    .catch((err)=>{
+      console.log("Error getting details from a single book from the DB", err);
+      next(err);
+    })
+})
 
 module.exports = router;
